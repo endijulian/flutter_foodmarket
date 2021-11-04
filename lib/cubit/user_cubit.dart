880e40a3177b837bf6c4454046db7690203fb7dd
endi julian <endi@iglobal.co.id>
2021-11-04 15:28:14 +0700

    update login failed

--- lib/cubit/user_cubit.dart
+++ lib/cubit/user_cubit.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:foodmarket/cubit/cubit.dart';
 import 'package:foodmarket/models/models.dart';
 import 'package:foodmarket/services/services.dart';
 
@@ -11,10 +14,31 @@ class UserCubit extends Cubit<UserState> {
   Future<void> signIn(String email, String password) async {
     ApiReturnValue<User> result = await UserServices.signIn(email, password);
 
-    if (result != null) {
+    if (result.value != null) {
       emit(UserLoaded(result.value));
     } else {
       emit(UserLoadingFailed(result.message));
+    }
+  }
+
+  Future<void> signUp(User user, String password, {File pictureFile}) async {
+    ApiReturnValue<User> result =
+        await UserServices.signUp(user, password, pictureFile: pictureFile);
+
+    if (result.value != null) {
+      emit(UserLoaded(result.value));
+    } else {
+      emit(UserLoadingFailed(result.message));
+    }
+  }
+
+  Future<void> uploadProfilePicture(File pictureFile) async {
+    ApiReturnValue<String> result =
+        await UserServices.uploadProfilePicture(pictureFile);
+
+    if (result.value != null) {
+      emit(UserLoaded((state as UserLoaded).user.copyWith(
+          picturePath: "http://127.0.0.1:8000/storage/" + result.value)));
     }
   }
 }
